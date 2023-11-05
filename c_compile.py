2673d63b8ae00310fc2c0c745ebf61671187d536
crazyjerz <informatyka11111@wp.pl>
2023-11-05 22:28:50 +0100

    FIXUP! correct linux binary

--- c_compile.py
+++ c_compile.py
@@ -5,6 +5,6 @@ import sys
 if sys.platform.startswith('win'):
     os.system("x86_64-w64-mingw32-gcc -shared -o src/static/c/bmp64lib.dll src/static/c/bmp64lib.c -O3")
 elif sys.platform.startswith('linux'):
-    os.system("gcc -shared -o src/static/c/bmp64lib.so -fPIC src/static/c/bmp64lib.c -O3")
+    os.system("gcc -shared -o src/static/c/bmp64lib.so -fPIC src/static/c/bmp64lib.c -O3 -Wno-unused-result")
 else:
     print("stop using this javastation")
