04b19a87fe0606e17f58934810f94aa823542aad
crazyjerz <informatyka11111@wp.pl>
2023-11-06 19:51:41 +0100

    rudimentary error handling: - "rudimentary" is code for "horrifically ugly" - moved some css files to their correct folders

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,11 +1,22 @@
 from flask_login import current_user
-from flask import render_template, Blueprint, request, redirect
+from flask import render_template, Blueprint, request, redirect, url_for
 from sqlalchemy import text
 import re
-from src import db
+from src import db, app
 
 main_bp = Blueprint('main', __name__, template_folder='templates', static_folder="static", static_url_path='/main-static')
 @main_bp.route("/")
 def main() -> str:  # main page render function isolated in file to get current status
     temp_username = current_user.username if current_user.is_authenticated else ""
     return render_template("main.html", username=temp_username)
+
+# error handlers
+@app.errorhandler(404)
+def teapot_error(e):
+    return render_template("error_handler.html", error=404, content="This site does not exist")
+@app.errorhandler(403)
+def teapot_error(e):
+    return render_template("error_handler.html", error=403, content="You are not logged in or you have attempted to access files that are not yours")
+@app.errorhandler(418)
+def teapot_error(e):
+    return render_template("error_handler.html", error=418, content="I'm a teapot")
