bea6943499ef67e9a7ac7d41e3e675c395fec6a1
crazyjerz <informatyka11111@wp.pl>
2023-11-06 20:09:37 +0100

    improvements to error handling: - can handle error 400, 500 (explicitly) - can handle all other errors

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,5 +1,6 @@
+import werkzeug
 from flask_login import current_user
-from flask import render_template, Blueprint, request, redirect, url_for
+from flask import render_template, Blueprint, request, redirect, url_for, abort
 from sqlalchemy import text
 import re
 from src import db, app
@@ -11,12 +12,40 @@ def main() -> str:  # main page render function isolated in file to get current 
     return render_template("main.html", username=temp_username)
 
 # error handlers
+@main_bp.route("/error/<path:error>")
+def check_error(error):
+    try:
+        abort(int(error))
+    except werkzeug.exceptions.HTTPException:
+        abort(int(error))
+    except Exception:
+        abort(400)
+
 @app.errorhandler(404)
 def error_404(e):
-    return render_template("error_handler.html", error=404, content="This site does not exist")
+    return render_template("error_handler.html", error=404, content=
+    "This site does not exist")
+
 @app.errorhandler(403)
 def error_403(e):
-    return render_template("error_handler.html", error=403, content="You are not logged in or you have attempted to access files that are not yours")
+    return render_template("error_handler.html", error=403, content=
+    "You are not logged in or you have attempted to access files that are not yours.")
+
 @app.errorhandler(418)
 def error_418(e):
-    return render_template("error_handler.html", error=418, content="I'm a teapot")
+    return render_template("error_handler.html", error=418, content=
+    "I'm a teapot.")
+
+@app.errorhandler(400)
+def error_400(e):
+    return render_template("error_handler.html", error=400, content=
+    "You have entered nonsensical data into your request.")
+
+@app.errorhandler(500)
+def error_500(e):
+    return render_template("error_handler.html", error=500, content=
+    "the error is on the server side. Refer to our GitHub issues page.")
+
+@app.errorhandler(Exception)
+def generic_error(e):
+    return render_template("error_handler.html", error=e.code, content=e.description)
