cf572817b6e62620faed5f2cecc9388eb876b8a3
FlyinButter1 <gabriellisiecki1@gmail.com>
2023-11-07 19:12:30 +0100

    fix gpt prompt

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -19,18 +19,18 @@ def test_api():
         return ""
 
     response = openai.ChatCompletion.create(
-    model="gpt-4",
+    model="gpt-3.5-turbo",
     messages=[
         {
         "role": "system",
-        "content": "Jesteś nauczycielem, który tworzy test na podstawie notatek. test podaj jako json według wzoru '[{\"pytanie\":\"content\", \"a\":\"content\", \"b\":\"content\", \"c\":\"content\", \"d\":\"content\", \"poprawna\":\"content\"} , ...]'. Podaj od 4 do 8 pytań.\n"        },
+        "content": "Jesteś nauczycielem, który tworzy test na podstawie notatek. test podaj jako json według wzoru '[{\"pytanie\":\"content\", \"a\":\"content\", \"b\":\"content\", \"c\":\"content\", \"d\":\"content\", \"poprawna\":\"content\"} , ...]'."        },
         {
         "role": "user",
         "content": f"{note.content}"
         }
     ],
     temperature=1,
-    max_tokens=3000,
+    max_tokens=4000,
     top_p=1,
     frequency_penalty=0.2,
     presence_penalty=0
